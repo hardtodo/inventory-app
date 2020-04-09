925878edfe86bd1856ab92cb9691d6b02c275322
YH <928895139@qq.com>
2020-04-09 15:33:56 +0800

    2020/4/9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
+import { Product } from './product';
+
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title :string;
-  imgUrl:string;
-
+  products:Product[];
   constructor()
   {
-    this.title='hello';
-    this.imgUrl='/assets/imgs/ng-book2.png';
+    this.products = [ 
+      new Product( 
+      'MYSHOES', 
+      'Black Running Shoes', 
+      '/assets/imgs/products/black-shoes.jpg', 
+      ['Men', 'Shoes', 'Running Shoes'], 
+      109.99), 
+      new Product( 
+      'NEATOJACKET', 
+      'Blue Jacket', 
+      '/assets/imgs/products/blue-jacket.jpg', 
+      ['Women', 'Apparel', 'Jackets & Vests'], 
+      238.99), 
+      new Product( 
+      'NICEHAT', 
+      'A Nice Black Hat', 
+      '/assets/imgs/products/black-hat.jpg', 
+      ['Men', 'Accessories', 'Hats'], 
+      29.99) 
+      ];
+  }
+
+  onProductSelected(product:Product)
+  {
+    console.log('产品:'+product.name+'选中');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { Student } from './student.component';
-import { StudentList } from './student-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list.component';
+import { ProductPriceComponent } from './product-price.component';
+import { ProductRowComponent } from './product-row.component';
+import { ProductImageComponent } from './product-image.component';
+import { ProductDepartmentComponent } from './product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentList,
-    Student
+    ProductDepartmentComponent,
+    ProductImageComponent,
+    ProductRowComponent,
+    ProductPriceComponent,
+    ProductsListComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
